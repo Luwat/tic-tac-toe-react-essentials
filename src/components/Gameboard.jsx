4642823a69ecd87c073ff0f830c16c7a9074323a
luwat <oluwatobioreniyi@gmail.com>
2024-01-19 15:12:06 +0100

    Lift state up

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 const initialGameBoard = [
     [null, null, null],
@@ -5,17 +6,19 @@ const initialGameBoard = [
     [null, null, null],
 ];
 
-export default function Gameboard() {
+export default function Gameboard({ onSelectSquare, isActivePlayer }) {
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
     function handleSelectSquare(rowIndex, colIndex) {
         setGameBoard((prevGameBoard) => {
             const updatedGameboard = [...prevGameBoard.map(innerArray => [...innerArray])];
 
-            updatedGameboard[rowIndex][colIndex] = 'X';
+            updatedGameboard[rowIndex][colIndex] = isActivePlayer;
 
             return updatedGameboard;
         })
+
+        onSelectSquare();
     }
     return (
         <ol id="game-board">
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react"
 
 
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
     const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState();
 
@@ -21,7 +21,7 @@ export default function Player({ initialName, symbol }) {
     }
 
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
             <span className="player">
                 {editPlayerName}
                 <span className="player-symbol">{symbol}</span>
